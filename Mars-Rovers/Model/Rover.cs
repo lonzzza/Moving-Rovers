a1d6199c18c0256010049c196f4f879f64cee11f
lonzzza <76197257+lonzzza@users.noreply.github.com>
2020-12-19 21:23:51 +0200

    Add files via upload

---
+++ Mars-Rovers/Model/Rover.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mars_Rovers.Model
+{
+    class Rover
+    {
+        private int positionX;
+
+        private int positionY;
+
+        private string compassHeading; 
+
+        public int PositionX 
+        {
+            get { return positionX; }
+            set { positionX = value; }
+        }
+
+        public int PositionY
+        {   
+            get { return positionY; }
+            set { positionY = value; }
+        }
+
+        public string CompassHeading
+        {
+            get { return compassHeading; }
+            set { compassHeading = value; }
+        }
+    }
+}
